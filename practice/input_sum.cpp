f0aae44ee4cc25549d0a93c1aee2562afdc6d418
TH-Raju <rajukhan894200@gmail.com>
2024-04-15 17:14:44 +0600

    User Input and sum

---
+++ practice/input_sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amount1;
+    cout<<"Mom gives: ";
+    cin>>amount1;
+
+
+    int amount2;
+    cout<<"Dad gives: ";
+    cin>>amount2;
+
+    int total_amount = amount1 + amount2;
+    cout<<"Total amount: "<<total_amount<<endl;
+    
+    
+    return 0;
+}
+
