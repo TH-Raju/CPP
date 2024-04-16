9e15f66836ed6ac0b01935cfeb46f9a88dc099d0
TH-Raju <rajukhan894200@gmail.com>
2024-04-16 19:22:15 +0600

    [Practice] Loops

---
+++ practice/Loops.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // // for loop
+    // int n;
+    // cin>>n;
+
+    // int sum = 0;
+    // for(int count=0;count<=n;count++){
+    //     sum += count;
+    // }
+
+
+    // cout<<sum<<endl;
+
+
+    // // while loop
+
+    // int n;
+    // cin>>n;
+
+    // while(n<10){
+    //     cout<<n<<endl;
+    //     n++;
+    // }
+
+
+    // do while loop
+
+    int n;
+    cin>>n;
+
+    do{
+        cout<<n<<endl;
+        cin>>n;
+        
+    }while(n>0);
+
+
+
+
+
+
+    return 0;
+}
