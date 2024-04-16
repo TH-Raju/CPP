1a4bf131334d33b69475ecdb90fa4581d1126249
TH-Raju <rajukhan894200@gmail.com>
2024-04-16 19:04:57 +0600

    [Practice] If else condition

---
+++ practice/if-else.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    // int savings;
+    // cout <<"How much money you have?: ";
+    // cin>>savings;
+
+    // if(savings>5000){
+    //     cout<<"Go with Neha"<<endl;
+    // }else{
+    //     cout<<"Go with Reshma"<<endl;
+    // }
+
+
+    // int a,b,c;
+    // cin>>a>>b>>c;
+
+
+    // if(a>b){
+    //     if(a>c){
+    //         cout<<a<<endl;
+    //     }else{
+    //         cout<<c<<endl;
+    //     }
+    // }else{
+    //     if(b>c){
+    //         cout<<b<<endl;
+
+    //     }else{
+    //         cout<<c<<endl;
+    //     }
+    // }
+
+
+    int n;
+    cin>>n;
+
+    if(n%2 == 0){
+        cout<<"Even"<<endl;
+    }else{
+        cout<<"Odd"<<endl;
+    }
+
+
+
+
+    return 0;
+}
